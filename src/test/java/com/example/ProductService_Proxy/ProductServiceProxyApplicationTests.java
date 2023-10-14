d034fef83e48e6580f04ca8442e090a357d88e56
mohanbecse2002 <mohanbbecse@gmail.com>
2023-10-14 14:11:21 +0530

    initial spring

---
+++ src/test/java/com/example/ProductService_Proxy/ProductServiceProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProductService_Proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
